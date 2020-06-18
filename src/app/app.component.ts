102db3f74bf221f797a318aa467ad349a238410f
glevon <levon77777@gmai.com>
2020-06-19 01:09:39 +0400

    deploy2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,44 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'tnayin1';
+  mardik=[
+    {
+      anun:"Inga",
+      azganun:"Grigoryan",
+      tariq:28,
+      ashxatavard:70000,
+      lezuner:["ruseren"]
+    },
+    {
+      anun:"Narek",
+      azganun:"Makaryan",
+      tariq:25,
+      ashxatavard:150000,
+      lezuner:["hayeren","ruseren","angleren"]
+    },
+    {
+      anun:"Artur",
+      azganun:"Babayan",
+      tariq:26,
+      ashxatavard:100000,
+      lezuner:["hayeren","ruseren","angleren"]
+    },
+    {
+      anun:"Diana",
+      azganun:"Sargsyan",
+      tariq:50,
+      ashxatavard:300000,
+      lezuner:["hayeren","ruseren","angleren","parskeren","franseren"]
+    }
+  ]
+  plus(x){
+    x.ashxatavard+=5000
+  }
+  minus(x,i){
+    x.ashxatavard-=5000
+    
+  }
 }
+
